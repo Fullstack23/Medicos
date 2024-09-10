099324d6788e2e676dafa3713369236c480a5300
Heitor <full54078@gmail.com>
2024-09-10 20:28:33 -0300

    Commit 5
    Adicionando novas pastas de paciente e consulta.

---
+++ medicos-main/medicos-main/api/src/main/java/com/senacshoes/api/model/consulta/Prioridade.java
@@ -0,0 +1,14 @@
+package com.senacshoes.api.model.consulta;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+public enum Prioridade {
+    BAIXA(0, "BAIXA"),
+    MEDIA(1, "MEDIA"),
+    ALTA(2, "ALTA");
+
+    private Integer codico;
+    private String descricao;
+}
